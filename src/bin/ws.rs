3734f665db9cc8c18ab92dff091a3eb390fd7bb6
Dustin Rohde <dustin.rohde@gmail.com>
2018-07-30 10:02:50 -0400

    misc(ws): misc improvements

--- src/bin/ws.rs
+++ src/bin/ws.rs
@@ -9,8 +9,9 @@ use conway::{Cell, Game, Grid, GridConfig, View};
 
 static WS_ADDR: &str = "localhost:3012";
 
-static CHAR_ALIVE: char = '■';
-static CHAR_DEAD: char = '□';
+// Grid defaults.
+const CHAR_ALIVE: char = '■';
+const CHAR_DEAD: char = '□';
 static DEFAULT_PATTERN: &str = r#"
 .......
 ...x...
@@ -18,6 +19,14 @@ static DEFAULT_PATTERN: &str = r#"
 ..xxx..
 .......
 "#;
+
+// Commands.
+const CMD_PING: &str = "ping";
+const CMD_STEP: &str = "step";
+const CMD_TOGGLE_PLAYBACK: &str = "toggle-playback";
+const CMD_SCROLL: &str = "scroll";
+const CMD_STOP: &str = "stop";
+const CMD_RESTART: &str = "restart";
 
 struct Server {
     out: ws::Sender,
@@ -35,14 +44,9 @@ impl Server {
     }
 
     fn new_game() -> Game {
-        let char_alive = CHAR_ALIVE.to_string();
-        let char_dead = CHAR_DEAD.to_string();
         Game::new(
             Grid::from_config(GridConfig {
-                pattern: DEFAULT_PATTERN
-                    .replace('x', char_alive.as_str())
-                    .replace('.', char_dead.as_str())
-                    .to_string(),
+                pattern: DEFAULT_PATTERN.to_owned(),
                 char_alive: CHAR_ALIVE,
                 char_dead: CHAR_DEAD,
                 view: View::Fixed,
@@ -55,41 +59,57 @@ impl Server {
         )
     }
 
+    fn set_game(&mut self, game: Game) {
+        let mutex = Arc::get_mut(&mut self.game).unwrap();
+        *mutex.get_mut().unwrap() = game;
+    }
+
     fn alert<T: fmt::Display + Into<ws::Message>>(&self, msg: T) -> ws::Result<()> {
         write!(stderr(), "{}", msg)?;
         self.out.send(msg)
     }
+
+    fn next_turn(&self, game: &mut Game) -> ws::Result<()> {
+        if game.is_over() {
+            self.alert("Game over.")
+        } else {
+            game.tick();
+            self.out.send(game.draw())
+        }
+    }
 }
 
 impl ws::Handler for Server {
+    fn on_open(&mut self, _: ws::Handshake) -> ws::Result<()> {
+        self.set_game(Server::new_game());
+        Ok(())
+    }
+
     fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
         let mut game = self.game.lock().unwrap();
 
         let mut args = msg.as_text()?.trim().splitn(2, ' ');
         match args.next() {
-            Some("ping") => {
+            Some(cmd) if cmd == CMD_PING => {
                 if self.paused {
                     return Ok(());
                 }
-                game.tick();
-                self.out.send(game.draw())
+                self.next_turn(&mut game)
             }
-            Some("step") => {
+            Some(cmd) if cmd == CMD_STEP => {
                 if !self.paused {
                     return Ok(());
                 }
-                game.tick();
-                self.out.send(game.draw())
+                self.next_turn(&mut game)
             }
-            Some("toggle-playback") => {
+            Some(cmd) if cmd == CMD_TOGGLE_PLAYBACK => {
                 self.paused = !self.paused;
                 if !self.paused {
-                    game.tick();
-                    return self.out.send(game.draw());
+                    return self.next_turn(&mut game);
                 }
                 Ok(())
             }
-            Some("scroll") => {
+            Some(cmd) if cmd == CMD_SCROLL => {
                 let Cell(dx, dy): Cell = match args.next().unwrap_or_default().parse::<Cell>() {
                     Ok(delta) => delta,
                     Err(err) => return self.alert(format!("WARNING: {}", err)),
@@ -97,18 +117,16 @@ impl ws::Handler for Server {
                 game.scroll(dx, dy);
                 self.out.send(game.draw())
             }
+            Some(cmd) if cmd == CMD_STOP => self
+                .out
+                .send("Game of Life ended. Select Restart to play again."),
+            // Some(cmd) if cmd == CMD_RESTART => {}
             Some(arg) => self.alert(format!(
                 "WARNING: message contained unexpected command '{}'",
                 arg
             )),
             None => self.alert("WARNING: empty message received"),
         }
-    }
-
-    fn on_open(&mut self, _: ws::Handshake) -> ws::Result<()> {
-        let mutex = Arc::get_mut(&mut self.game).unwrap();
-        *mutex.get_mut().unwrap() = Server::new_game();
-        Ok(())
     }
 }
 
